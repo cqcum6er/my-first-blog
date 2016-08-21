596a23071877c6bee85bbf7fbc3fd0a648e834be
ericsun <ericsun1221@yahoo.com>
2016-08-21 11:25:09 -0700

    Use Canadian YF and added 'DJ_list.csv' to the list of files to ignore by Git.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post  #'Post' table object is retrieved from 'models.py' within the same folder.
 import requests
 import csv
+import datetime
 
 def post_list(request):  #Home url
 	return render(request, 'blog/home.html')
@@ -9,157 +10,49 @@ def post_list(request):  #Home url
 def inProgrss(request):  #In-Progress url
 	return render(request, 'blog/InProgress.html')
 
-def KeyFet(sep,num):  #KeyFet() function fetches components of key statistics page; att=attribute, sep=separator, num=Nth separator
-	att = ""
-	for ch in html.split(sep)[num]:
-		if ch != "<":
-			att += ch
-		else:
-			break #terminates the nearest enclosing loop when '<' is ran into (the loop control target, ch, keeps its current value).
-	return att
-
 def DJ_list(request):  #"post_list" must be requested from urls.py
 	if Post:  #Check to see if there is existing entry.  If there is, delete existing entry to prepare for updated entry.
 		Post.objects.all().delete()
 	else:
 		pass
-	with open('DJ_list.csv', 'rb') as file:
+	with open('DJ_list.csv', 'rb') as file:  # Need to use absolute path when on Pythonanywhere server (i.e. use '/home/cqcum6er/my-first-blog/DJ_list.csv' as file path.)
 		infile = csv.reader(file, delimiter=",", quotechar='"')
-		next(infile, None)  # skip the headers
-		for row in infile:
+		#next(infile, None)  #Skip column headers.
+		print type(infile)
+		last_date = ""
+		for row in reversed(list(infile)):  #Read csv file in reverse order (must convert csv reader object to list format 1st.)
 			#if row.line_num == 1:
-				#continue  #Skip column header.
-			post = Post(
-			Symbol=row[0],
-			LastPrice=row[1],
-			FiftyTwoWkChg=row[2],
-			FiftyTwoWkLo=row[3],
-			FiftyTwoWkHi=row[4],
-			DivYild=row[5],
-			TrailPE=row[6],
-			ForwardPE=row[7],
-			PEG_Ratio=row[8],
-			PpS=row[9],
-			PpB=row[10],
-			Market_Cap=row[11],
-			Free_Cash_Flow=row[12],
-			Market_per_CashFlow=row[13],
-			Enterprise_per_EBITDA=row[14],
-			Name=row[15],
-			)
-			post.save()  #Save each entry ('post') of database.
+				#continue  #Skip column header; not needed with csv.reader?.
+			todayTag = row[0]
+			current_date = datetime.datetime.strptime(todayTag,'%Y-%m-%d').date()  #Convert str to time format and extract content in the format of "yyyy-mm-dd".  Use .date() to request just the date.
+			current_date = str(current_date)  #Convert 'datetime.date' object to string for comparison.
+			print current_date, type(current_date)
+			print last_date, type(last_date)
+			if current_date == last_date or last_date == "":  #Faster processing if put 'last_date == ""' last since it's only processed at the beginning row; keep writing out key statistics until date changes from today.
+				post = Post(
+				Symbol=row[1],  #Skip 1st date column (row[0]).
+				LastPrice=row[2],
+				FiftyTwoWkChg=row[3],
+				FiftyTwoWkLo=row[4],
+				FiftyTwoWkHi=row[5],
+				DivYild=row[6],
+				TrailPE=row[7],
+				ForwardPE=row[8],
+				PEG_Ratio=row[9],
+				PpS=row[10],
+				PpB=row[11],
+				Market_Cap=row[12],
+				Free_Cash_Flow=row[13],
+				Market_per_CashFlow=row[14],
+				Enterprise_per_EBITDA=row[15],
+				Name=row[16],
+				)
+				post.save()  #Save each entry ('post') of database.
+			else:
+				break  #Exit the smallest 'for' loop.
+			last_date = current_date  #Save the current
+			print current_date
+			print last_date
 		posts = Post.objects.values()  #values() returns content of database as dictionary, thus making the database iterable.
-		return render(request, 'blog/DJ.html', {'posts': posts})  #To serve as a template, 'blog/post_list.html' has to be put in blog\template\blog\
+		return render(request, 'blog/DJ.html', {'posts': posts})  #To serve as a template, 'blog/DJ.html' has to be put in blog\template\blog\
 		#The last parameter, which looks like this: {} is a place to integrate objects in models.py (posts) with html ('posts') in template folder.
-	'''
-	#posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	global html  #Make 'html' accessible to KeyFet function.
-	response = requests.get('http://finance.yahoo.com/q/cp?s=%5EDJI+Components')
-	html = response.text  #Convert requests object to string.
-	DJlist = []
-	for iter in xrange(5,35):
-		DJsym = ""
-		for ch in html.split('/q?s=')[iter]:
-			if ch != "\"":
-				DJsym += ch
-			else:
-				break #Continue next iteration in outer 'for' loop when '"' is ran into.
-		DJlist.append(DJsym.encode('ascii'))
-	#for tick in DJlist:
-		#YFconn.request('GET','/q/ks?s='+tick+'+Key+Statistics')
-	if Post:  #Check to see if there is existing entry.  If there is, delete existing entry to prepare for updated entry.
-		Post.objects.all().delete()
-	else:
-		pass
-	for tick in DJlist:
-		#YFconn.request('GET','/q/ks?s='+tick+'+Key+Statistics')
-		response = requests.get('http://finance.yahoo.com/q/ks?s='+tick+'+Key+Statistics')
-		html = response.text  #Convert requests object to string.
-		if 'tabledata1\">' in html:
-			LastPrice = ""
-			if 'yfs_l84_' in html:
-				for ch in html.split('yfs_l84_')[1]:
-					if ch != '<':
-						LastPrice += ch
-					else:
-						break
-				LP_Split = LastPrice.split('>')
-				LP = LP_Split[1].replace(',','')
-				print LP
-			_52WkChg = ""
-			for ch in html.split('tabledata1\">')[33]:
-				if ch != "<":
-					_52WkChg += ch
-				else:
-					break
-			_52WkLo = KeyFet('tabledata1\">',36)
-			_52WkHi = KeyFet('tabledata1\">',35)
-			Div = KeyFet('tabledata1\">',53)
-			tPE = KeyFet('tabledata1\">',3)
-			fPE = KeyFet('tabledata1\">',4)
-			PEG = KeyFet('tabledata1\">',5)
-			PpS = KeyFet('tabledata1\">',6)
-			PpB = KeyFet('tabledata1\">',7)
-			MktCap = ""
-			if 'yfs_j10_' in html:
-				for ch in html.split('yfs_j10_')[1]:
-					if ch != '<':
-						MktCap += ch
-					else:
-						break
-				MC_Split = MktCap.split('>')
-				MktCap = MC_Split[1]
-			else:
-				MktCap = KeyFet('tabledata1\">',1)
-			FCF = KeyFet('tabledata1\">',31)
-			if MktCap[-1] == "T":
-				MCflo = float(MktCap[:-1])*10**6
-			elif MktCap[-1] == "B":
-				MCflo = float(MktCap[:-1])*1000
-			elif MktCap[-1] == "M":
-				MCflo = float(MktCap[:-1])
-			elif MktCap[-1] == "K":
-				MCflo = float(MktCap[:-1])/1000
-			else:
-				continue
-			if FCF[-1] == "B":
-				FCFflo = float(FCF[:-1])*1000
-				MpC = str(MCflo/FCFflo)
-			elif FCF[-1] == "M":
-				FCFflo = float(FCF[:-1])
-				MpC = str(MCflo/FCFflo)
-			elif FCF[-1] == "K":
-				FCFflo = float(FCF[:-1])/1000
-				MpC = str(MCflo/FCFflo)
-			else:
-				MpC = "N/A"
-			EpE = KeyFet('tabledata1\">',9)
-			Name = ""
-			for ch in html.split('<h2>')[2]:
-				if ch != "<":
-					Name += ch
-				else:
-					break
-		post = Post(
-			Symbol=tick,
-			LastPrice=LP,
-			FiftyTwoWkChg=_52WkChg,
-			FiftyTwoWkLo=_52WkLo,
-			FiftyTwoWkHi=_52WkHi,
-			DivYild=Div,
-			TrailPE=tPE,
-			ForwardPE=fPE,
-			PEG_Ratio=PEG,
-			PpS=PpS,
-			PpB=PpB,
-			Market_Cap=MktCap,
-			Free_Cash_Flow=FCF,
-			Market_per_CashFlow=MpC,
-			Enterprise_per_EBITDA=EpE,
-			Name=Name,
-			)
-		post.save()  #Save each entry of database.
-	posts = Post.objects.values()  #values() returns content of database as dictionary, thus making the database iterable.
-	return render(request, 'blog/DJ.html', {'posts': posts})  #To serve as a template, 'blog/post_list.html' has to be put in blog\template\blog\
-	#The last parameter, which looks like this: {} is a place to integrate objects in models.py (posts) with html ('posts') in template folder.
-	'''
