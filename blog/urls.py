17a0ff651e8e8a6207430f533959883c321bf221
ericsun <ericsun1221@yahoo.com>
2017-01-08 11:23:25 -0800

    Added html template for LastMnth/LastQtr/LastYr; QuerySet status is now shown with each template above table.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views  #Import "views.py" in the current folder.
 urlpatterns = [
     url(r'^$', views.home, name='home_link'),  #r'^$' indicates the start of the string and end of string regex symbols with nothing between them; 'http://127.0.0.1:8000/' is not a part of URL.
 	url(r'^DJ_LastDay/', views.DJ_LastDay, name='DJ_LastDay'),  #Include 'name' parameter for easy link reference in html file.
-	url(r'^DJ_LastWeek/', views.DJ_LastWk, name='DJ_LastWk'),  #Include 'name' parameter for easy link reference in html file.
+	url(r'^DJ_LastWeek/', views.DJ_LastWk, name='DJ_LastWk'),
+	url(r'^DJ_LastMonth/', views.DJ_LastMnth, name='DJ_LastMnth'),
+	url(r'^DJ_LastQuarter/', views.DJ_LastQtr, name='DJ_LastQtr'),
+	url(r'^DJ_LastYear/', views.DJ_LastYr, name='DJ_LastYr'),
 	url(r'^inProgress/', views.inProgrss, name ='inProgress_link'),  #For pages under construction.
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post  #'Post' table object is retrieved from 'models.py' within the same folder.
-from django.db.models import Max
 import requests
 import csv
 import datetime
 import aniso8601
 '''
-#Save scheduled csv record from Pythonanywhere, then run local server ONCE to populate local database before commenting out this block; comment out this block before saving views.py to Pythonanywhere to use scheduled csv instead...
+#To update database with the current date, save scheduled csv record from Pythonanywhere, then run local server ONCE to populate local database before commenting out this block; comment out this block before saving views.py to Pythonanywhere to use scheduled csv instead...
 #if not Post:  #Check to see if database is empty.  If it's not, do nothing, else empty existing entry to prepare for update. (Remove the 'if' statement if run as a scheduler command on Pythonanywhere.)
 	#pass
 #else:
@@ -20,9 +19,9 @@ with open('DJ_list.csv', 'rb') as file:  # Need to use absolute path when on Pyt
 #...end of block.
 '''
 def iso_to_gregorian(iso_year, iso_week, iso_day):  #Converts ISO week date format to Gregorian calendar date format.
-    jan4 = datetime.date(iso_year, 1, 4)  #1st week ('Week 01') of new year always contains Jan 4th.
-    start = jan4 - datetime.timedelta(days=jan4.isoweekday()-1)
-    return start + datetime.timedelta(weeks=iso_week-1, days=iso_day-1)
+	jan4 = datetime.date(iso_year, 1, 4)  #1st week ('Week 01') of new year always contains Jan 4th.
+	start = jan4 - datetime.timedelta(days=jan4.isoweekday()-1)  #Get the year for ISO week date.
+	return start + datetime.timedelta(weeks=iso_week-1, days=iso_day-1)
 
 def home(request):  #Home url
 	return render(request, 'blog/home.html')
@@ -36,24 +35,67 @@ def DJ_LastDay(request):  #"DJ_LastDay" must be requested from urls.py
 	last_date = datetime.date(2016, 9, 17)
 	posts = Post.objects.filter(Day__range=(first_date, last_date))'''
 	#posts = Post.objects.filter(Day__lte=timezone.now()).exclude(Day__lte=timezone.now() - datetime.timedelta(days=1))  #Retrieve all dates equal to or older than today but exclude those from yesterday or older; use timezone.now() instead of datetime.datetime.now() to avoid problems with timezones.
-	#posts = Post.objects.values('Day').order_by('-Day').annotate(Max('Day'))
 	p = Post.objects.latest('Day')  #Returns an object instance (not iterable); if latest() is empty, it works with attributes defined by 'class Meta' in models.py. Note latest () only retrieve ONE instance; .values() needs to be inserted in front of latest() to make it iterable as dictionary.
 	posts = Post.objects.filter(Day=p.Day)  #Retrieve all instances with latest day ('p.Day') as QuerySet object.
 	'''print type(p.Day)
-	print p.Day
-	print timezone.now()
-	print iso_to_gregorian(timezone.now().isocalendar()[0], timezone.now().isocalendar()[1], 3)  #Convert Gregorian calendar date format to ISO week date format.
-	testday = datetime.datetime.strptime('2017-01-01','%Y-%m-%d')  #Convert str to datetime object from datetime module.
-	print testday
-	print iso_to_gregorian(testday.isocalendar()[0], testday.isocalendar()[1]-1, 3)  #Show date from last week's Wednesday.'''
-	return render(request, 'blog/DJ_LastDay.html', {'posts': posts})  #To serve as a template, 'blog/DJ.html' has to be put in blog\template\blog\
+	print p.Day'''
+	'''TestDay = datetime.datetime.strptime('2017-01-06','%Y-%m-%d')
+	print TestDay
+	print iso_to_gregorian(TestDay.isocalendar()[0], TestDay.isocalendar()[1]-1, 3)'''
+	#print (aniso8601)
+	return render(request, 'blog/DJ_LastDay.html', {'posts': posts})#, {'LastDay': p.Day})  #To serve as a template, 'blog/DJ.html' has to be put in blog\template\blog\
 	#The last parameter, which looks like this: {} is a place to integrate objects in models.py (posts) with html ('posts') in template folder.
 
 def DJ_LastWk(request):  #Display value from Wednesday of last week.
-	p = Post.objects.latest('Day')  #Returns an object instance (not iterable); if latest() is empty, it works with attributes defined by 'class Meta' in models.py. Note latest () only retrieve ONE instance; .values() needs to be inserted in front of latest() to make it iterable as dictionary.
+	p = Post.objects.latest('Day')
 	LastDay = datetime.datetime.strptime(str(p.Day),'%Y-%m-%d')  #Convert str to datetime object from datetime module.
-	'''print LastDay
+	print LastDay
 	print iso_to_gregorian(LastDay.isocalendar()[0], LastDay.isocalendar()[1]-1, 3)  #Show date from last week's Wednesday.'''
 	posts = Post.objects.filter(Day=iso_to_gregorian(LastDay.isocalendar()[0], LastDay.isocalendar()[1]-1, 3))  #Retrieve all instances from Wednesday of last week.
-	return render(request, 'blog/DJ_LastWk.html', {'posts': posts})  #To serve as a template, 'blog/DJ.html' has to be put in blog\template\blog\
-	#The last parameter, which looks like this: {} is a place to integrate objects in models.py (posts) with html ('posts') in template folder.
+	return render(request, 'blog/DJ_LastWk.html', {'posts': posts})
+
+def DJ_LastMnth(request):  #Display value from the 1st (trading) day of last month.
+	p = Post.objects.latest('Day')
+	LastDay = datetime.datetime.strptime(str(p.Day),'%Y-%m-%d')
+	#print type(LastDay.strftime('%m'))
+	print LastDay
+	CurrYr = LastDay.strftime('%Y')  #Retrieve current month as string from datetime object, LastDay.
+	CurrMnth = LastDay.strftime('%m')  #Retrieve current month as string from datetime object, LastDay.
+	if int(CurrMnth) >= 2:  #Check whether last month is still within the same year.
+		p = Post.objects.filter(Day__year=CurrYr, Day__month=str(int(CurrMnth)-1)).earliest('Day')  #Retrieve earliest date available from last month.
+		print p.Day
+		posts = Post.objects.filter(Day=p.Day)
+	else:
+		p = Post.objects.filter(Day__year=str(int(CurrYr)-1), Day__month=str(int(CurrMnth)+11)).earliest('Day')
+		print p.Day
+		posts = Post.objects.filter(Day=p.Day)
+	return render(request, 'blog/DJ_LastMnth.html', {'posts': posts})
+
+def DJ_LastQtr(request):  #Display value from the 1st (trading) day of last quarter.
+	p = Post.objects.latest('Day')
+	LastDay = datetime.datetime.strptime(str(p.Day),'%Y-%m-%d')
+	print LastDay
+	#print type(LastDay.strftime('%m'))
+	CurrYr = LastDay.strftime('%Y')
+	CurrMnth = LastDay.strftime('%m')
+	if int(CurrMnth) >= 4:  #Check whether last quarter is still within the same year.
+		p = Post.objects.filter(Day__year=CurrYr, Day__month=str(int(CurrMnth)-3)).earliest('Day')
+		print p.Day
+		posts = Post.objects.filter(Day=p.Day)
+	else:
+		p = Post.objects.filter(Day__year=str(int(CurrYr)-1), Day__month=str(int(CurrMnth)+9)).earliest('Day')
+		print p.Day
+		posts = Post.objects.filter(Day=p.Day)
+	return render(request, 'blog/DJ_LastQtr.html', {'posts': posts})
+
+def DJ_LastYr(request):  #Display value from the 1st (trading) day of last year.
+	p = Post.objects.latest('Day')
+	LastDay = datetime.datetime.strptime(str(p.Day),'%Y-%m-%d')
+	print LastDay
+	#print type(LastDay.strftime('%m'))
+	CurrYr = LastDay.strftime('%Y')
+	CurrMnth = LastDay.strftime('%m')
+	p = Post.objects.filter(Day__year=str(int(CurrYr)-1)).earliest('Day')  #Retrieve earliest date available from last year, doesn't matter the month.
+	print p.Day
+	posts = Post.objects.filter(Day=p.Day)
+	return render(request, 'blog/DJ_LastYr.html', {'posts': posts})
